97b4d595ee3f8831580f1abd68742336be9c4f2c
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-01-01 21:40:06 +0100

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -13,5 +13,19 @@ module.exports = {
     },
     compress: true,
     port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
